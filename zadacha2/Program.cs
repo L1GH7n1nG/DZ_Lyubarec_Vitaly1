179415225416d1edef97fdbfdbafd6587b6954ea
L1GH7n1nG <l1ghtawp1@gmail.com>
2023-08-02 08:02:18 +0300

    finished

--- zadacha2/Program.cs
+++ zadacha2/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write("Введите целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b) Console.Write($"max = {a}");
+else Console.Write($"max = {b}");
