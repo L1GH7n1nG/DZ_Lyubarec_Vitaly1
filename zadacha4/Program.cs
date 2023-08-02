b4a47c0994a847055c842e420fca50b3b913fe82
L1GH7n1nG <l1ghtawp1@gmail.com>
2023-08-02 08:02:22 +0300

    finished

--- zadacha4/Program.cs
+++ zadacha4/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Введите целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b && a > c) Console.Write($"max = {a}");
+else if (b > a && b > c) Console.Write($"max = {b}");
+else if (c > a && c > b) Console.Write($"max = {c}");
+else if (a == c || a == b) Console.Write($"max = {a}");
+else if (b == c) Console.Write($"max = {b}");
